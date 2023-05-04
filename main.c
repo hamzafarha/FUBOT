7ca8d890f54daa116e63105929aebb5140470f4c
yossra-baazaoui <baazaouiyossra@gmail.com>
2023-05-05 00:11:47 +0100

    modification

--- main.c
+++ main.c
@@ -8,14 +8,13 @@
 #include "enigme.h"
 #include "background.h"
 #include "personne.h"
-#define COUNTDOWN_TIME 20 // in seconds
-#define DELAY_AFTER_INCORRECT_RESPONSE 5000 // 5 seconds
+#define COUNTDOWN_TIME 20
+#define DELAY_AFTER_INCORRECT_RESPONSE 5000 
 
 int main() {
   int quit = 1;
   int countCorrect=0;
-  char selectedAnswer[2] = {0}; // this variable will hold the user's selected answer
-  // add a boolean flag to keep track of whether the win sound has been played
+  char selectedAnswer[2] = {0}; 
   bool hasPlayedWinSound = false;
   bool hasPlayedLoseSound = false;
   bool hasWon = false;
@@ -25,9 +24,9 @@ int main() {
   if (SDL_Init(SDL_INIT_VIDEO) != 0) { printf("SDL_Init failed: %s\n", SDL_GetError());return 1;}
   screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
   if (screen == NULL){printf("Unable to set video mode: %s\n", SDL_GetError());return 1;}
-   // initialize SDL
+ 
   SDL_Init(SDL_INIT_AUDIO);
-  // initialize SDL_mixer
+
   if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) == -1) {printf("Failed to initialize SDL mixer: %s\n", Mix_GetError());return 1;}
     //
     playMusic("sound/music.mp3");
@@ -38,11 +37,10 @@ int main() {
     Uint32 startTime = SDL_GetTicks();
     //
     Uint32 lastIncorrectTime = 0;
-    bool canClick = true; // track whether the user is allowed to click on an answer or not
-    Uint32 clickTimer = 0; // track the time since the user last clicked on an answer
+    bool canClick = true; 
+    Uint32 clickTimer = 0; 
     bool canClickNow = false;
-    //
-    // Initialize the personnage Forn function sauvegarder()
+    
     Personne p;
     p.position.x = 0;
     p.position.y = 0;
@@ -58,24 +56,17 @@ int main() {
     p.vie = 3;
     p.sens = 0;
     p.isjumping = 0;
-    // Initialize the background
+  
     background b;
     b.camerapos.x = 0;
     b.camerapos.y = 0;
     b.camerapos.w = 640;
     b.camerapos.h = 480;
     b.direction = 1;
-    /*b.image1 = IMG_Load("bg1.png");
-    b.image2 = IMG_Load("bg2.png");
-    b.image3 = IMG_Load("bg3.png");
-    b.music1 = Mix_LoadMUS("music1.mp3");
-    b.music2 = Mix_LoadMUS("music2.mp3");
-    b.music3 = Mix_LoadMUS("music3.mp3");*/
+    
     b.musicplaying = 0;
 
-    ///////////////////////////////// Save and load the data    (.bin file to prevent user --> modify data)
-    //sauvegarder(p, b, "game.bin");
-    //////////////////////////////// Charger 
+   
     Personne pppp;
     background bbbb;
     charger(&pppp, &bbbb, "game.bin");
@@ -103,36 +94,35 @@ int main() {
         int timeLeft = COUNTDOWN_TIME - (currentTime - startTime) / 1000;
         if (timeLeft < 0 && !hasWon && !gameover) {
             printf("Time's up! Game over.\n");
-            //quit = 0;
-            //break;
+            
             timeLeft=0;
             gameover=true;
         }
 
         if (!hasWon && !hasPlayedLoseSound) {
-        afficherTemps(e, screen, timeLeft); // Timer
-        afficherProgress(e,screen,countCorrect); //  3/0
+        afficherTemps(e, screen, timeLeft); 
+        afficherProgress(e,screen,countCorrect); 
         }
         
         if(gameover && !hasPlayedLoseSound)
         {
          ImageGameOver(screen);
          playMusic("sound/gameover.mp3");
-         hasPlayedLoseSound = true; // set the flag to true so the sound won't play again
+         hasPlayedLoseSound = true; 
         }
         
         if(countCorrect==3 && !hasPlayedWinSound)
         {
-        startTime = SDL_GetTicks(); // reset the countdown timer
+        startTime = SDL_GetTicks(); 
         ImageGameWin(screen);
         playMusic("sound/win.mp3");
-        hasPlayedWinSound = true; // set the flag to true so the sound won't play again
+        hasPlayedWinSound = true; 
         }
         //
         if(canClickNow && !hasWon && !gameover)
         {
            Uint32 currentTimeee = SDL_GetTicks();
-           //printf("Please wait %d seconds before trying again.\n", (clickTimer + 5000 - currentTimeee) / 1000);
+          
            int waitTime = (clickTimer + 5000 > currentTimeee) ? ((clickTimer + 5000 - currentTimeee) / 1000) : 0;
            afficherWait(e, screen, waitTime);            
         }      
@@ -143,26 +133,26 @@ int main() {
                     if (canClickNow) {
                         break;
                     }
-                // Check if the mouse click is inside one of the answer rectangles
+                
        if (!gameover && !hasWon && event.button.button == SDL_BUTTON_LEFT) {
          if (event.button.x >= e.positionReponse1.x && event.button.x <= (e.positionReponse1.x + 290) && event.button.y >= e.positionReponse1.y && event.button.y <= e.positionReponse1.y+45)  {
                         printf("A\n");
-                        strcpy(selectedAnswer, "A"); // the user selected A
+                        strcpy(selectedAnswer, "A"); 
                     }
           else if (event.button.x >= e.positionReponse2.x && event.button.x <= (e.positionReponse2.x + 290) && event.button.y >= e.positionReponse2.y && event.button.y <=(e.positionReponse2.y + 45)) {
                         printf("B\n");
-                        strcpy(selectedAnswer, "B"); // the user selected B
+                        strcpy(selectedAnswer, "B"); 
                     }
            else if (event.button.x >= e.positionReponse3.x && event.button.x <= (e.positionReponse3.x + 290) && event.button.y >= e.positionReponse3.y && event.button.y <= (e.positionReponse3.y + 45)){
                         printf("C\n");
-                        strcpy(selectedAnswer, "C"); // the user selected C
+                        strcpy(selectedAnswer, "C"); 
                     }
            else if (event.button.x >= e.positionReponse4.x && event.button.x <= (e.positionReponse4.x + 290) && event.button.y >= e.positionReponse4.y && event.button.y <= (e.positionReponse4.y + 45)) {
                         printf("D\n");
-                        strcpy(selectedAnswer, "D"); // the user selected D
+                        strcpy(selectedAnswer, "D");
                     }
               else {
-                        // The click was outside of any answer rectangle, so don't attempt to answer
+                         answer
                         continue;
                     }
                     
@@ -172,17 +162,16 @@ int main() {
                             {
                             genererEnigme(&e, "enigme.txt");
                             }
-                            //afficherEnigme(e, screen);
+                            
                             if(countCorrect<3)
                             {
                             countCorrect++;
                             }
                             if(countCorrect==3 && !hasWon) {
-                            // User has won
+                            
                             hasWon = true;
                             }
-                            //printf("Count Correct %d \n",countCorrect);
-                            //startTime = SDL_GetTicks(); // reset the countdown timer
+                           
                             PlaySoundEffect("sound/correct.wav");
                         } else {
                             PlaySoundEffect("sound/wrong.wav");
@@ -192,7 +181,7 @@ int main() {
                             canClickNow= true;
                             clickTimer = currentTimesss;
                         }
-                        strcpy(selectedAnswer, ""); // reset selectedAnswer for next round
+                        strcpy(selectedAnswer, "");
                 }
 
                 break;
@@ -212,9 +201,9 @@ int main() {
                 hasWon = false;
                 gameover = false;
                 countCorrect=0;
-                startTime = SDL_GetTicks(); // reset the countdown timer
+                startTime = SDL_GetTicks(); 
                 playMusic("sound/music.mp3");
-                //clickTimer = SDL_GetTicks(); // reset click timer
+                
                 canClickNow=false;
               }
             }
@@ -225,7 +214,7 @@ int main() {
             }
         }
     }  
-        // check if the delay after an incorrect answer has passed
+        
         if (canClickNow) {
             Uint32 currentTimemm = SDL_GetTicks();
             if (currentTimemm - lastIncorrectTime >= DELAY_AFTER_INCORRECT_RESPONSE) {
@@ -235,7 +224,7 @@ int main() {
         SDL_Flip(screen);
 }
   free_all(e);
-  SDL_FreeSurface(screen);// free the memory
+  SDL_FreeSurface(screen);
   SDL_Quit();
   return 0;
 }
